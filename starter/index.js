6b8266dd6454d0bb0cbdc182e931fa6bb4b14d17
Elide <zavalavinagreelide@gmail.com>
2021-12-26 10:08:45 -0700

    replace

--- starter/index.js
+++ starter/index.js
@@ -31,6 +31,18 @@ const url = require('url');
 
 ////////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%IMAGE%}/g, product.IMAGE);
+}
+
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8'); 
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8'); 
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8'); 
@@ -46,7 +58,7 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-type': 'text/html'});
         res.end('Estamos en este momento comunicanonos por overview');
 
-        const cardHtml = dataObj.map(el => repleceTemplate(tempCard))
+        const cardHtml = dataObj.map(el => repleceTemplate(tempCard, el))
 9
         res.end(tempOverview);
 
